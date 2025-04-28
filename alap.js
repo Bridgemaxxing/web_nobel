914f1e38a889eec8e5904b1f9fc4a3a377a56b52
Bridgemaxxing <marthajozsef2@gmail.com>
2025-04-28 08:21:08 +0200

    sima kezelgetes

--- alap.js
+++ alap.js
@@ -2,22 +2,26 @@ fetch("https://api.nobelprize.org/v1/prize.json")
 .then(x=>x.json())
 .then(y=>megjelenit(y))
 
-
 function megjelenit(y){
     console.log(y)
-
     let sz=""
-    for (const elem of y.prizes){
+    let sorszam=0
+    for (const elem of y.prizes) {
         sz+=`
-        <div>
-        <p>${elem.year}</p>
-        <p>${elem.category}</p>
+        <div class="col-sm-3">
+            <div class="szegely" data-bs-toggle="modal" data-bs-target="#myModal" onclick="reszletKiir(${sorszam})" >
+                <p>${elem.year}</p>
+                <p>${elem.category}</p>
+            </div>
         </div>
-        `
-    }
 
+        `
+        sorszam++
+    }
     document.getElementById("keret").innerHTML=sz
 
+}
 
-
+function reszletKiir(sorszam){
+    console.log(sorszam)
 }
